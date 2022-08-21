14eb900aa28e5c45b03f19bad9d57120139173eb
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-21 18:28:40 +0100

    reduced the width: :

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const infoContainer = document.querySelector(".info")
 const shareContainer = document.querySelector(".share")
 const shareButton = document.getElementById("share-image")
+const closeButton = document.getElementById("close-image")
 
 shareButton.addEventListener("click", () => {
     shareContainer.classList.remove("hidden")
     infoContainer.style.display = "none"
 });
 
+closeButton.addEventListener("click", () => {
+    shareContainer.style.display = "none"
+    infoContainer.classList.remove("hidden")
+});
